be254daa9827911a91372427e25a6c5cbfafa1d5
remy <clerge@gmail.com>
2023-10-09 13:37:59 +0200

    création de vueErreur.php et modification de l'index

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
     require 'Modele.php';
+    try {
     $billets = getBillets();
     // Affichage
     $contenu = 'vueAccueil.php';
     require 'gabarit.php';
+    } catch (Exception $e) {
+        $msgErreur = $e->getMessage();
+	$contenu = 'vueErreur.php' ;
+	require 'gabarit.php' ;
+    }
 ?>
-                    
+
