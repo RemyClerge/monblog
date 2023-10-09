b320dfae220ac78a5797f1f15ab4dd03b9d50200
remy <clerge@gmail.com>
2023-10-09 15:05:02 +0200

    ajout des function dans Modele.php

--- Modele.php
+++ Modele.php
@@ -1,6 +1,6 @@
 <?php
         function getBillets(){
-	$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8','root', '');
+	$bdd = getBdD();
     	$billets = $bdd->query('select idBillet as id, dateBillet as date,'
             . ' titreBillet as titre, contenuBillet as contenu from BILLET'
             . ' order by idBillet desc');
@@ -11,4 +11,24 @@
                                                   array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
                 return $bdd;
         }
+
+        function getBillet($idBillet){
+                $bdd = getBdD();
+                $billet = $bdd->prepare('select * from billet WHERE idBillet =:idBillet');
+                $billet->bindParam(':idBillet', $idBillet);
+                $billet->execute();
+                $res = $billet->fetch(PDO::FETCH_ASSOC);
+                return $res;
+        }
+
+        function getCommentaires($idBillet){
+                $bdd = getBdD();
+                $com = $bdd->query('select idCommentaire as id, dateCommentaire as date,'
+                                . ' auteurCommentaire as auteur, contenuCommentaire as contenu from COMMENTAIRE'
+                                . ' where idBillet=' . $idBillet);
+                return $com;
+        
+        }
+
+        
 ?>
