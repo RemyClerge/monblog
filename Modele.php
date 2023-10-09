bef82318974c32967be853212a065a73f7c9a8c3
remy <clerge@gmail.com>
2023-10-09 13:46:28 +0200

    ajout du suivi des dossier.php

---
+++ Modele.php
@@ -0,0 +1,14 @@
+<?php
+        function getBillets(){
+	$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8','root', '');
+    	$billets = $bdd->query('select idBillet as id, dateBillet as date,'
+            . ' titreBillet as titre, contenuBillet as contenu from BILLET'
+            . ' order by idBillet desc');
+        return $billets;
+        }
+        function getBdD() {
+                $bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8','root','',
+                                                  array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                return $bdd;
+        }
+?>
